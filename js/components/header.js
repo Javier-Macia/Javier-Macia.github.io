ec9a47ab1e4e044b4f86cf8992cd07e018b1f4e5
Javier <javier.macia05@goumh.umh.es>
2023-07-16 18:16:17 +0200

    Added the reusability of the header

---
+++ js/components/header.js
@@ -0,0 +1,41 @@
+class Header extends HTMLElement {
+    constructor(active = "") {
+        super();
+
+        this.active_page = this.getAttribute('active-page') || active;
+    }
+
+    connectedCallback() {
+        console.log(this.active_page);
+        this.innerHTML = `
+        <header>
+            <input type="checkbox" id="nav-toggle">
+            <label for="nav-toggle" class="nav-toggle-label">
+                <div>
+                    <hr/>
+                    <hr/>
+                    <hr/>
+                </div>
+            </label>
+            <nav>
+                <ul>
+                    <li class="nav-item ${this.active_page === '' ? 'active' : ''}">
+                        <a href=" ${this.active_page === '' ? '.' : '..'}">Home</a>
+                    </li>
+                    <li class="nav-item ${this.active_page === 'about' ? 'active' : ''}">
+                        <a href=" ${this.active_page === '' ? '.' : '..'}/about/">About</a>
+                    </li>
+                    <li class="nav-item ${this.active_page === 'projects' ? 'active' : ''}">
+                        <a href=" ${this.active_page === '' ? '.' : '..'}/projects/">My projects</a>
+                    </li>
+                    <li class="nav-item ${this.active_page === 'education' ? 'active' : ''}">
+                        <a href=" ${this.active_page === '' ? '.' : '..'}/education/">Education</a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+        `;
+    }
+}
+
+customElements.define('header-component', Header);

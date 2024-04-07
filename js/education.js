f325c6daa9f4506ba235ddeb4fd587845d00eb21
Javier <javier.macia05@goumh.umh.es>
2024-04-07 18:16:09 +0200

    New page

--- js/education.js
+++
@@ -1,4 +0,0 @@
-const summaryContainers = document.querySelectorAll('.summary-container');
-summaryContainers.forEach((container, index) => {
-  container.style.animationDelay = `${index * 1.4}s`;
-});
